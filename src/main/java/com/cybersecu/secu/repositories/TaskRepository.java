03ea366086aece91cc3b246b1a073826d2225df7
tchawou-daniel <daniel-jordan.sipe-tchawou@lacatholille.fr>
2021-02-09 01:16:48 +0100

    modify the getOne function

--- src/main/java/com/cybersecu/secu/repositories/TaskRepository.java
+++ src/main/java/com/cybersecu/secu/repositories/TaskRepository.java
@@ -2,6 +2,10 @@ package com.cybersecu.secu.repositories;
 
 import com.cybersecu.secu.models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Integer> {
 }
